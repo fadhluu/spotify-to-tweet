2a93ac9d94e950b278f26e656cdb291e46a0bda1
fadhlu <fadhlurahman.i@gmail.com>
2020-05-01 22:32:09 +0700

    Init

---
+++ bot.py
@@ -0,0 +1,22 @@
+import tweepy
+
+
+class Twitter:
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
+        self.auth = self.authentication()
+        self.api = tweepy.API(self.auth)
+
+    def authentication(self):
+        self.auth = tweepy.OAuthHandler(
+            self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
+        return self.auth
+
+    def post_tweet(self, text, file_location):
+        text = f"I'm currently listening to:\n" + text
+        self.api.update_with_media(filename=file_location, status=text)
+        print(f"Success tweeting: {text}")
